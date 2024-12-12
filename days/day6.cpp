0f60a53222dea9c90ff91b14e49cf95ec037aa3b
Josh Ghiloni <ghiloni@gmail.com>
2024-12-11 21:05:45 -0500

    day6 part2 wip

--- days/day6.cpp
+++ days/day6.cpp
@@ -1,8 +1,48 @@
 #include <iostream>
 #include "base.hpp"
 #include "day6.hpp"
+#include <map>
 
 using namespace std;
+
+
+const map<char, coordinate> adjustments = {
+  {'^', coordinate(0,-1)},
+  {'v', coordinate(0,1)},
+  {'<', coordinate(-1, 0)},
+  {'>', coordinate(1, 0)}
+};
+
+const map<char, char> rotated = {
+  {'^', '>'},
+  {'>', 'v'},
+  {'v', '<'},
+  {'<', '^'}
+};
+
+coordinate potentialObstacle(coordinate corner1, coordinate corner2, coordinate corner3, char guard) {
+  int c4x, c4y;
+
+  if (corner1.first == corner2.first) {
+    c4x = corner3.first;
+  } else if (corner1.first == corner3.first) {
+    c4x = corner2.first;
+  } else if (corner2.first == corner3.first) {
+    c4x = corner1.first;
+  }
+
+  if (corner1.second == corner2.second) {
+    c4y = corner3.second;
+  } else if (corner1.second == corner3.second) {
+    c4y = corner2.second;
+  } else if (corner2.second == corner3.second) {
+    c4y = corner1.second;
+  }
+
+  coordinate adjustment = adjustments.at(guard);
+
+  return coordinate(c4x + adjustment.first, c4y + adjustment.second);
+}
 
 long Day6::part1(istream& in) {
   dotMatrix screen;
@@ -11,7 +51,8 @@ long Day6::part1(istream& in) {
 
   printDotMatrix(screen);
 
-  coordinate position = this->findGuard(screen);
+  pair<coordinate, char> guard = this->findGuard(screen);
+  coordinate position = guard.first;
   while (this->isOnScreen(screen, position)) {
     position = this->nextPosition(screen, position);
     printDotMatrix(screen);
@@ -29,19 +70,58 @@ long Day6::part1(istream& in) {
 }
 
 long Day6::part2(istream& in) {
-  return 0L;
+  dotMatrix screen;
+  getDotMatrix(in, &screen);
+  long total = 0L;
+
+  printDotMatrix(screen);
+
+  pair<coordinate, char> guard = this->findGuard(screen);
+  coordinate corner1, corner2, corner3, newObstacle;
+
+  rotationPoint next = this->nextRotationPoint(screen, guard.first, guard.second);
+  corner1 = next.location;
+
+  next = this->nextRotationPoint(screen, corner1, next.newAlignment);
+  corner2 = next.location;
+
+  next = this->nextRotationPoint(screen, corner2, next.newAlignment);
+  corner3 = next.location;
+
+  newObstacle = potentialObstacle(corner1, corner2, corner3, next.newAlignment);
+
+  while (this->isOnScreen(screen, newObstacle)) {
+    screen.matrix[newObstacle.second][newObstacle.first] = 'O';
+    total++;
+
+    corner1 = corner2;
+    corner2 = corner3;
+
+    next = this->nextRotationPoint(screen, corner2, next.newAlignment);
+    corner3 = next.location;
+
+    if (corner3 == NOT_FOUND) {
+      break;
+    }
+
+    newObstacle = potentialObstacle(corner1, corner2, corner3, next.newAlignment);
+  }
+
+  printDotMatrix(screen);
+
+  return total;
 }
 
-coordinate Day6::findGuard(dotMatrix screen) {
+pair<coordinate, char> Day6::findGuard(const dotMatrix& screen) {
   for (int y = 0; y < screen.y; y++) {
     for (int x = 0; x < screen.x; x++) {
       if (screen.matrix[y][x] == '^' || screen.matrix[y][x] == 'v' || screen.matrix[y][x] == '<' || screen.matrix[y][x] == '>') {
-        return coordinate(x, y);
+        return pair(coordinate(x, y), screen.matrix[y][x]);
       }
     }
   }
 
-  return NOT_FOUND;
+  return pair(NOT_FOUND, '.');
 }
 
 coordinate Day6::nextPosition(dotMatrix& screen, coordinate current) {
@@ -82,7 +162,34 @@ coordinate Day6::nextPosition(dotMatrix& screen, coordinate current) {
   return next;
 }
 
-bool Day6::isOnScreen(dotMatrix screen, coordinate position) {
+bool Day6::isOnScreen(const dotMatrix& screen, coordinate position) {
   return (position.first >= 0 && position.first < screen.x) &&
     (position.second >= 0 && position.second < screen.y);
+}
+
+rotationPoint Day6::nextRotationPoint(const dotMatrix& screen, coordinate currentPosition, char guard) {
+  coordinate adjustment = adjustments.at(guard);
+
+  coordinate position = currentPosition;
+  while (position != NOT_FOUND) {
+    coordinate previous = position;
+    position = coordinate(position.first + adjustment.first, position.second + adjustment.second);
+    if (screen.matrix[position.second][position.first] == '#') {
+      return rotationPoint{
+        .location = previous,
+        .oldAlignment = guard,
+        .newAlignment = rotated.at(guard),
+      };
+    }
+
+    if (!this->isOnScreen(screen, position)) {
+      break;
+    }
+  }
+
+  return rotationPoint{
+    .location = NOT_FOUND,
+    .oldAlignment = guard,
+    .newAlignment = 'X',
+  };
 }
--- days/day6.hpp
+++ days/day6.hpp
@@ -6,8 +6,14 @@
 
 using namespace std;
 
-typedef pair<size_t, size_t> coordinate;
+typedef pair<int, int> coordinate;
 const coordinate NOT_FOUND = coordinate(-1, -1);
+
+typedef struct rotationPoint {
+  coordinate location;
+  char oldAlignment;
+  char newAlignment;
+} rotationPoint;
 
 class Day6 : public AdventOfCodeDay {
 public:
@@ -15,8 +21,9 @@ public:
   long part2(istream& in);
 private:
   coordinate nextPosition(dotMatrix& matrix, coordinate position);
-  coordinate findGuard(dotMatrix matrix);
-  bool isOnScreen(dotMatrix screen, pair<size_t, size_t> position);
+  rotationPoint nextRotationPoint(const dotMatrix& matrix, coordinate currentPosition, char currentGuardAlignment);
+  pair<coordinate, char> findGuard(const dotMatrix& matrix);
+  bool isOnScreen(const dotMatrix& screen, coordinate position);
 };
 
 #endif
